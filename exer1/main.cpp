cbef206fbb1c64f7f7c54336f3dc37f69aedde4d
Copyright <copyright1999>
2021-09-08 23:06:21 +0800

    first demo

---
+++ exer1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void main_output(void)
+{
+    cout<<"this output sentence is from main.cpp"<<endl;
+}
+
+int main(void)
+{
+    main_output();
+    return 0;
+}
